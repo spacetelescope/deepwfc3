af46fa75528f5f5ad4f97b0ab4a102c2687929cb
Megan Jones <mjones@stsci.edu>
2024-02-26 17:19:35 -0500

    Make corrections to original data processing file

--- projects/uvis_gs_fail/data_process.py
+++ projects/uvis_gs_fail/data_process.py
@@ -8,98 +8,97 @@ from astropy.io import fits
 import tqdm
 from skimage.transform import resize, rotate
 
-
-
 def log_image_process(fpath, orig_data=False):
     '''
+    Apply the logarithmic image processing pipeline to an image.
+
     Opens a flt.fits file and logarithmically processes the data according to 
     the following method:
-    - opens the flt file, extracts SCI array, and creates an array of image 
-    data.
-    - Sets all values in the image array that are less than one to be equal to 
-    1.
-    - Scales the image data logarithmically.
-    - Uses min/max scaling to scale the pixel values to be between 0 and 1. 
-    - Resizes the image array to the dimensions (256, 256).
+        - Opens the SCI array of the flt file, and creates an array of image 
+        data.
+        - Sets all values in the image array that are less than one to be equal 
+        to 1.
+        - Scales the image data logarithmically.
+        - Resizes the image array to the dimensions (256, 256).       
+        - Uses min/max scaling to scale the pixel values to be between 0 and 1. 
 
-    Parameters:
+    Parameters
     -----------
-    fpath: string
-    The string that identifies the directory that the image is located in.
+    fpath : string
+        The path to the location of image to be processed. 
+    
+    orig_data : bool
+        Specify whether or not to also return the original image data. Default
+        value: False
 
-    Returns: numpy array of floats
+    Returns
     --------
-    log_data: array of floats
-    Array of logarithmically scaled image data.
+    scaled_image : array of floats
+        The processed image.
 
-    rootname: string
-    A String containing the rootname of the image that was processed.
+    image_data : array of floats
+        The original, pre-processed image data. Returned if orig_data==True
     '''
-
-     #Unpacking the image data: 
-        # get the boolean value of the subarray
+    # Get the boolean value of the subarray
     subarray_value = fits.getval(fpath, 'subarray')
     
-    # If the subarray is false then it is full chip, so we get the 
-    # two data arrays, minus the 6 dead rows of pixels
+    # If the subarray is false then it is full chip, so we get the two data
+    # arrays, minus the 6 dead rows of pixels
     if subarray_value == False:
         data1 = fits.getdata(fpath,'sci', 1)[:-3]
         data2 = fits.getdata(fpath, 'sci', 2)[3:]
         
         image_data = np.vstack((data1, data2))
-        
     else:
-        
         image_data = fits.getdata(fpath, 'sci', 1)
     
-        # Clip low values 
-    image_copy = image_data.copy() # Copy image_data array
+    # Clip low values 
+    # Copy image_data array
+    image_copy = image_data.copy()
 
-    pos_mask = image_copy < 1 # make positive mask
-    image_copy[pos_mask] = 1 # Make all values positive
+    # make positive mask
+    pos_mask = image_copy < 1 
+
+    # Make all values positive
+    image_copy[pos_mask] = 1 
         
-        # log scale data 
+    # Log scale data 
     log_data = np.log10(image_copy)
             
-        # Resize log scaled image 
+    # Resize log scaled image 
     log_data = resize(log_data, (256,256), order=3) 
     
-        # Min/max scale image array:
-    scaled_image = (log_data - log_data.min())/(log_data.max() 
-                                                  - log_data.min())
+    # Min/max scale image array:
+    scaled_image = (log_data - log_data.min())/\
+                   (log_data.max() - log_data.min())
     
     if orig_data == True:
-        return(scaled_image, image_data)
+        return scaled_image, image_data
     else:
-        return(scaled_image)
-    
+        return scaled_image
     
 def augment(image):
     '''
-    Takes in an array of image data, and performs data augmentation. The
-    image has a 50% chance of being vertically flipped, and then a 50%
-    chance of being horizontally flipped. Finally, a degree is chosen at
-    random from the range (0,360], the image is rotated to that angle, and then
-    the image is cropped to [c:c1,c:c1] where:
-    c = int(np.ceil((1/(np.sqrt(2)))*((np.sqrt(2)-1)/2)*256))
-    c1 = 256 - c
-    
-    The final image array will have the size (180, 180)
+    Performs data augmentation on image data.
 
-    Parameters:
+    Take in an image and perform the following operations to create an augmented
+    copy:
+        - Vertically flip the image with 50% probability
+        - Horizontally flip the image with 50% probability
+        - Rotate the image to a random degree of (0,360]
+        - Crop the image in the center to be (180,180)
+    
+    Parameters
     -----------
-    image: array of floats
-    Image data that needs to be augmented.
+    image : array of floats
+        Image data that needs to be augmented.
 
-    Returns:
+    Returns
     --------
-    augmented_image: array of floats
-    Augmented image data.
-
-    
+    augmented_image : array of floats
+        Augmented image data.
     '''
-    
-        # AUGMENT 1: VERTICAL FLIP
+    # AUGMENT 1: VERTICAL FLIP
     flip_chance = np.random.random(2)
     
     # start with 50/50 chance of vertical flip
@@ -111,7 +110,7 @@ def augment(image):
         # don't flip image
         augment1 = image
         
-        # AUGMENT 2: HORIZONTAL FLIP
+    # AUGMENT 2: HORIZONTAL FLIP
     # 50/50 chance of horizontal flip
     if flip_chance[1] >= 0.5:
         # flip image
@@ -120,9 +119,7 @@ def augment(image):
         # don't flip
         augment2 = augment1
         
-        
-        # AUGMENT 3: ROTATE AND CROP IMAGE
-
+    # AUGMENT 3: ROTATE AND CROP IMAGE
     # rotate image to some random degree in the range [0,360)
     R = np.random.random()
     rand_degree = (R*360) # degree value
@@ -134,75 +131,68 @@ def augment(image):
     c1 = 256 - c
     
     augmented_image = rotated_image[c:c1,c:c1] # cropped image
-    
-    
-        
-    return(augmented_image)
 
+    return augmented_image
 
 def process_augment(fpath):
     '''
-    Take in the path to an image file, process the image, augment the image 10 
-    times, and then save the image to the specified directory.
+    Take in the path to an image file, process the image, create 10 augmented 
+    copies of the image, and then save the image and its copies to the specified
+    directory.
 
-    Parameters:
+    Parameters 
     -----------
-    fpath: string
-    String specifying the path to the image file to be processed.
+    fpath : string
+        The path to the image file to be processed.
 
-    Returns:
+    Returns
     --------
-    None
+        none
 
     '''
-
+    # Get the rootname of the image:
     r1 = os.path.basename(fpath)
     r2 = r1.split('_')
     root = r2[0]
 
     processed_image = log_image_process(fpath)
-    
-    aug_images = [] # list of augmented images
+    # initialize empty list for augmented image
+    aug_images = [] 
 
     for i in range(0,10):
-        aug_images.append(augment(processed_image)) # augment image and add to 
-                                                    # image list
+        # augment image and add to image list
+        aug_images.append(augment(processed_image)) 
     
     # save the processed and augmented images to the specified directory
     np.savez_compressed(f'{save_dir}/{root}_processed.npz', 
                         no_aug=processed_image, aug=aug_images)
-    print('Processed rootname: ', root) # print the rootname that was processed
-
-    return()
-
-
+    
+    # print the rootname that was processed
+    print('Processed rootname: ', root) 
 
 def main(file_name):
     '''
-    Take in a path to a csv file, read the csv file in as a dataframe, and pool
+    Take in a path to a csv file, read the csv file, and pool
     the processing, augmentation, and saving processes of the images within that
     file.
 
-    Parameters:
+    Parameters
     -----------
-    file_name: string
-    String specifying the path to the csv file with the locations of the images
-    to be processed.
+    file_name : string
+        The path to the csv file with the locations of the images to be 
+        processed.
 
-    Returns:
+    Returns
     --------
-    None
-
+        none
     '''
-
-    # Read in dataframe
+    # Read in csv file as dataframe
     df = pd.read_csv(file_name)
 
-    # make list of the image locations
+    # Make list of the image locations
     image_locations = []
     
-
-    # get image locations
+    # Get image locations
     for i in range(len(df)):
         img_info = df.iloc[i]
         root = img_info['rootname']
@@ -212,27 +202,23 @@ def main(file_name):
         image_locations.append(fpath)
 
     total = len(image_locations)
-    # pooling 
-    pool = Pool(os.cpu_count())
+    # pooling image processing
+    cpu_count = int(0.75*(os.cpu_count()))
+    pool = Pool(cpu_count())
     list(tqdm.tqdm(pool.imap(process_augment, image_locations), total=total))
     pool.close()
     pool.join()
 
-    return()
-
-
 if __name__ == "__main__":
 
     file_name = str(sys.argv[1])
-    # file name is the full path to the csv file containing data
+    # file_name is the full path to the csv file containing data
     
     fname = os.path.basename(file_name)
 
-    # indicate the directory that the data should be saved to:
+    # Indicate the directory that the data should be saved to:
     save_dir = str(input("Please indicate the directory to save processed data \
                           to: "))
-    # saving to the appropriate directory 
-    main(file_name) # Run main function
-
-
+    # Run main function
+    main(file_name) 
 
